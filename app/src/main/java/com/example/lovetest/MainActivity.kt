01e45f1cacfd1164ed117fad16cda4bf7349d248
ChoHJin <cjc9739@naver.com>
2022-02-21 17:35:12 +0900

    finish
    끝

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = nav_host_fragment.findNavController()
+
+        btn_next.setOnClickListener{
+            navController.navigate(R.id.action_mainFragment_to_questionFragment)
+        }
     }
 }
